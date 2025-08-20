1e71f1d0fc8b1d31ef134fa08d00cd13e498951f
shamshu <shamshuushaashh@gmail.com>
2025-08-20 13:59:48 +0530

    changed the initialaization way

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -3,12 +3,16 @@ const admin = require('firebase-admin');
 
 const initializeFirebaseAdmin = () => {
   try {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     // serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
 
     console.log('Firebase Admin SDK initialized successfully');
   } catch (error) {
