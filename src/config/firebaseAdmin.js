f842dd40f58c2350ea15b8172de3b543facc8dbe
shamshu <shamshuushaashh@gmail.com>
2025-08-20 13:45:34 +0530

    removed programatically the extra lines

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 const initializeFirebaseAdmin = () => {
   try {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+    // serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
